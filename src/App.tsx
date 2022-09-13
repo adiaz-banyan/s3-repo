8aa6703c58594ca203638de38a7ac2cd46fb66dc
adiaz-banyan <86432508+adiaz-banyan@users.noreply.github.com>
2022-09-13 09:41:44 -0700

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import { useState } from "react";
-import axios from "axios";
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
   const [file, setFile] = useState<any>(null);
-  const [description, setDescription] = useState<string>("");
+  const [description, setDescription] = useState<string>('');
   const [images, setImages] = useState<any[]>([null]);
+  const [currentImage, setCurrentImage] = useState();
 
   const onImageUpload = async (image: any, description: string) => {
     const fd = new FormData();
-    fd.append("image", image);
-    fd.append("description", description);
-    const result = await axios.post("/api/s3-url-images", fd, {
-      headers: { "Content-Type": "multipart/form-data" },
-    });
+    fd.append('image', image);
+    fd.append('description', description);
+    const result = await axios.post(
+      'http://localhost:8080/api/s3-url-images',
+      fd,
+      {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      }
+    );
     return result.data;
   };
 
   const onSubmit = async (e: any) => {
     e.preventDefault();
     const result = await onImageUpload(file, description);
+    setCurrentImage(result.imagePath);
     setImages([result.image, ...images]);
   };
 
@@ -27,25 +33,40 @@ function App() {
     setFile(file);
   };
 
+  console.log(currentImage);
+
   return (
-    <div className="App">
-      <header className="App-header">Amazon s3 bucket</header>
-      <div className="form-container">
-        <form className="image-form" onSubmit={onSubmit}>
+    <div className='App'>
+      <header className='App-header'>Amazon s3 bucket</header>
+      <div className='form-container'>
+        <form className='image-form' onSubmit={onSubmit}>
           <input
             onChange={fileSelected}
-            id="imageInput"
-            type="file"
-            accept="image/*"
+            id='imageInput'
+            type='file'
+            accept='image/*'
           />
           <input
-            type="text"
+            type='text'
             value={description}
             onChange={(e) => setDescription(e.target.value)}
           />
-          <button type="submit">Upload</button>
+          <button type='submit'>Upload</button>
         </form>
       </div>
+
+      {currentImage && (
+        <img
+          style={{ width: '200px', margin: '20px' }}
+          src={`http://localhost:8080/api${currentImage}`}
+          alt='just words. Im going mad'
+        />
+      )}
+
+      <img
+        src='http://localhost:8080/api/images/191f629bea02893c186b64f1d249e7ee'
+        alt='words of magic and shit'
+      />
     </div>
   );
 }
